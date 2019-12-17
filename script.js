e1575b3fd21d22eb7bc7c2652b5ddb69c7ff8a0f
Chellemussemissedoffedelle Young <chellemussemissedoffedelleyoung@samsung-smart-fridge.kea.local>
2019-12-17 11:52:02 +0100

    splash færdig

--- script.js
+++ script.js
@@ -31,5 +31,5 @@ function showSlides() {
     }
 
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
