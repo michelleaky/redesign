5489aed7b1cb73c80d29ce47387d5d6e0d7a92b8
Zainab Waleed <ZainabW@Air.local>
2019-12-17 12:04:37 +0100

    færdigebilleder

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", sidenVises);
+
+
+var slideIndex = 0;
+
+function sidenVises() {
+    console.log("Siden Vises");
+
+
+
+    showSlides();
+
+}
+
+function showSlides() {
+    var i;
+
+    var slides = document.getElementsByClassName("splashbilleder");
+    console.log('slides', slides);
+
+
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    console.log('slideIndex', slideIndex);
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
